63ea637cd27f1e1e211dadf61c37284b7981a179
sayuri kawasaki <sayuri.e1128@gmail.com>
2020-01-21 13:05:42 +0000

    First

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all
+  @topics = Topic.all.includes(:favorite_users)
   end
   
   def new
@@ -9,7 +9,7 @@ class TopicsController < ApplicationController
 
   def create
     @topic = current_user.topics.new(topic_params)
-
+    
     if @topic.save
       redirect_to topics_path, success: '投稿に成功しました'
     else
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,6 +7,8 @@ class Topic < ApplicationRecord
 
   mount_uploader :image, ImageUploader
   # has_secure_password
-
   # has_many :topics
+  
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: 'user'
 end
